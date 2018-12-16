d5a74a42f3b08f20c9d3eaf4ba0c934cc895c75e
Brian <bsilvers@yahoo.com>
2018-12-16 09:37:28 -0600

    Property Binding progress

--- Projects/4_6_project_files/app/components/users/users.component.ts
+++ Projects/4_6_project_files/app/components/users/users.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { User } from 'src/app/models/user';
+
+import { User } from '../../models/User'; 
 
 @Component({
   selector: 'app-users',
@@ -11,69 +12,59 @@ export class UsersComponent implements OnInit {
   showExtended: boolean = true;
   loaded: boolean = false;
   enableAdd: boolean = true;
-  currentClasses: {};
-  currentStyles: {};
+  currentClasses = {};
+  currentStyles = {};
 
   constructor() { }
 
   ngOnInit() {
-
-    
+   
       this.users = [
         {
           firstName: 'John',
-          lastName:  'Doe',
+          lastName: 'Doe',
           age: 70,
           address: {
-              street: '50 Main St.',
-              city: 'Boston',
-              state: 'MA'
+            street: '50 Main st',
+            city: 'Boston',
+            state: 'MA'
           },
           image: 'http://lorempixel.com/600/600/people/3',
           isActive: true
-      },
-      {
-        firstName: 'Kevin',
-        lastName:  'Neelin',
-        age: 34,
-        address: {
-            street: '20 School St.',
+        },
+        {
+          firstName: 'Kevin',
+          lastName: 'Johnson',
+          age: 34,
+          address: {
+            street: '20 School st',
             city: 'Lynn',
             state: 'MA'
+          },
+          image: 'http://lorempixel.com/600/600/people/2',
+          isActive: false
         },
-        image: 'http://lorempixel.com/600/600/people/2',
-        isActive: false
-    },
-    {
-      firstName: 'Karen',
-      lastName:  'Williams',
-      age: 26,
-      address: {
-          street: '55 Mill St.',
-          city: 'Miami',
-          state: 'FL'
-      },
-      image: 'http://lorempixel.com/600/600/people/1',
-      isActive: true
-  }
+        {
+          firstName: 'Karen',
+          lastName: 'Williams',
+          age: 26,
+          address: {
+            street: '55 Mill st',
+            city: 'Miami',
+            state: 'FL'
+          },
+          image: 'http://lorempixel.com/600/600/people/1',
+          isActive: true
+        }
       ];
+
       this.loaded = true;
 
+      // this.addUser({
+      //   firstName: 'David',
+      //   lastName: 'Jackson'
+      // });
 
-    
-    
-   // this.showExtended = false;
-
-    // this.addUser({
-    //   firstName: 'David',
-    //   lastName:  'Jackson' //,
-    //   // age: 44,
-    //   // address: {
-    //   //     street: '12 Wake St.',
-    //   //     city: 'Miami',
-    //   //     state: 'FL'
-    //  // }
-    // });
       this.setCurrentClasses();
       this.setCurrentStyles();
   }
@@ -95,5 +86,5 @@ export class UsersComponent implements OnInit {
       'font-size': this.showExtended ? '' : '40px'
     }
   }
-
+  
 }
