a44c1dda1bd6b9709fcd8c0727da175e2a95d134
Brian <bsilvers@yahoo.com>
2018-12-23 08:48:36 -0600

    Observables & Component Communication

--- Projects/devlogger/src/app/services/log.service.ts
+++ Projects/devlogger/src/app/services/log.service.ts
@@ -1,11 +1,20 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, of } from 'rxjs';
+
+
 import { Log } from '../models/Log';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class LogService {
   logs: Log[];
+
+  private logSource = new BehaviorSubject<Log>({id:null, text: null, date: null});
+  selectedLog = this.logSource.asObservable();
+
+
   constructor() { 
     this.logs = [
       {id: '1', text: 'Generated components', date: new Date('12/26/2018 12:54:23') },
@@ -15,7 +24,11 @@ export class LogService {
 
   }
 
-  getLogs() {
-    return this.logs;
+  getLogs(): Observable<Log[]> {
+    return of(this.logs);
+  }
+
+  setFormLog(log: Log) {
+    this.logSource.next(log);
   }
 }
